3619c42c0ed27ffd7dd1f644d34bfe7c4a445133
JuanRodrigoHabitzreuter <juanrhabitzreuter@gmail.com>
2023-08-22 22:06:17 -0300

    feat add if

---
+++ exercicios/exercicio9.js
@@ -0,0 +1,11 @@
+export function averages(grade1, grade2, grade3) {
+    return  ((grade1 + grade2 + grade3) / 3)
+
+    // if (averages >= 7) {
+    //     console.log(`Your grade is: ${averages.toFixed(2)} Approved`)
+    // } else if (averages >= 5) {
+    //     console.log(`Your grade is: ${averages.toFixed(2)} Recovery`)
+    // } else {
+    //     console.log(`Your grade is: ${averages.toFixed(2)} Disapproved`)
+    // }
+}
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import { convertKm } from "./exercicios/exercicio5.js";
 import { duration } from "./exercicios/exercicio6.js";
 import { convertMetersCentimeters } from "./exercicios/exercicio7.js";
 import { multiplicationTables } from "./exercicios/exercicio8.js";
-import { averages } from "./exercicios/desafio0.js";
+import { averages } from "./exercicios/exercicio9.js";
 
 
 const app = express();
@@ -119,21 +119,76 @@ app.post("/api/exercicio8", (req, res) => {
   })
 })
 
-app.post("/api/desafio0", (req, res) => {
 
-  const avarage = (req.body.average)
-  const grade1 = (req.body.grade1)
-  const grade2 = (req.body.grade2)
-  const grade3 = (req.body.grade3)
-
-  res.json({
-    message: `resultado ${averages(avarage,grade1,grade2,grade3)}`
+  app.post("/api/exercicio9", (req, res) => {
+    const average = req.body.average;
+    const grade1 = req.body.grade1;
+    const grade2 = req.body.grade2;
+    const grade3 = req.body.grade3;
+    const averages = (grade1 + grade2 + grade3) / 3;
+  
+    let resultMessage = "";
+    if (averages >= 7) {
+      resultMessage = `Your grade is: ${averages.toFixed(2)} Approved`;
+    } else if (averages >= 5) {
+      resultMessage = `Your grade is: ${averages.toFixed(2)} Recovery`;
+    } else {
+      resultMessage = `Your grade is: ${averages.toFixed(2)} Disapproved`;
+    }
+  
+    console.log(resultMessage);
+  
+    res.json({
+      message: `Resultado: ${averages.toFixed(2)}`,
+      gradeStatus: resultMessage
+    })
   })
-})
+  
+//   const average = req.body.average;
+//   const grade1 = req.body.grade1;
+//   const grade2 = req.body.grade2;
+//   const grade3 = req.body.grade3;
+//   const averages = (grade1 + grade2 + grade3) / 3;
+
+//   if (averages >= 7) {
+//     average = (`Your grade is: ${averages.toFixed(2)} Approved`);
+//   } else if (averages >= 5) {
+//     avarage = (`Your grade is: ${averages.toFixed(2)} Recovery`);
+//   } else {
+//     avarage = (`Your grade is: ${averages.toFixed(2)} Disapproved`);
+//   }
+
+//   res.json({
+//     message: `Resultado: ${[averages.toFixed(2),
+//     avarage]}`
+//   });
+// });
+
+
+// app.post("/api/exercicio9", (req, res) => {
+
+//   const avarage = (req.body.average)
+//   const grade1 = (req.body.grade1)
+//   const grade2 = (req.body.grade2)
+//   const grade3 = (req.body.grade3)
+//   const avarages = (grade1 + grade2 + grade3) / 3
+
+//   if (averages >= 7) {
+//         console.log(`Your grade is: ${averages.toFixed(2)} Approved`)
+//     } else if (averages >= 5) {
+//         console.log(`Your grade is: ${averages.toFixed(2)} Recovery`)
+//     } else {
+//         console.log(`Your grade is: ${averages.toFixed(2)} Disapproved`)
+//     }
+
+//   res.json({
+//     message: `resultado ${averages(avarage,grade1,grade2,grade3)}`
+//   })
+// })
 
 // app.get("/api/exercicio2", (req, res) => {
 
-//   //console.log(req)
+   //console.log(req)
 
 //   const num1 = parsefloat (req.query.num1)
 //   const num2 = parsefloat (req.query.num2)
@@ -145,21 +200,21 @@ app.post("/api/desafio0", (req, res) => {
 
 //  app.get("/", (req, res) => {
 
-//     //app.get("/", (_, res)
-//     //req: requisição
-//     //res: resposta
-// //   res.json({message: 'Olá Mundo!'});
+  //app.get("/", (_, res)
+ //req: requisição
+     //res: resposta
+//   res.json({message: 'Olá Mundo!'});
 //   res.json({message: 'Batata!'}); 
 // });
 
 // app.listen(port, ()  =>{
 //   console.log(`Servidor rodando na porta ${port}!`);
-//   //para visualizar no navegaor
-//   //localhost:3000/
+ //para visualizar no navegaor
+  //localhost:3000/
 // });
 
-// //desafio2 metodo get
-// //executar arquivo usando o comando:
+ //desafio2 metodo get
+//executar arquivo usando o comando:
 // app.get('/api/:userid/:nome', (req, res) => {
 //     const nome = req.query.nome;
 //     const id = req.params.userid;
